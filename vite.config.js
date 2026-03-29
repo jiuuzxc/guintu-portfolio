6e416aa54559ecd48562e50f081f3e1a5d30e583
Julian <jmm.guintu@gmail.com>
2026-03-29 22:29:24 +0800

    github pages configuration update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/guintu-portfolio/',
+
   assetsInclude: [
     '**/*.exe',
     '**/*.rpm',
